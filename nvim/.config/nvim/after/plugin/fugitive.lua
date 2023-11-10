f0f221f81e932e3c26ed321df431f7b985062ed1
Romain Trotard <romain.trotard@gmail.com>
2023-11-10 09:11:47 +0100

    Add shortcut for fugitive merge

--- nvim/.config/nvim/after/plugin/fugitive.lua
+++ nvim/.config/nvim/after/plugin/fugitive.lua
@@ -1,2 +1,6 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git);
 
+-- Solve merge conflict with fugitive
+vim.keymap.set("n", "gf", "<cmd>diffget //2<CR>");
+vim.keymap.set("n", "gj", "<cmd>diffget //3<CR>");
+
