092697019d7d2229c2bea49698e45c59115fbf3d
Romain Trotard <romain.trotard@gmail.com>
2024-11-07 12:50:36 +0100

    Add fugitive diff split

--- nvim/.config/nvim/after/plugin/fugitive.lua
+++ nvim/.config/nvim/after/plugin/fugitive.lua
@@ -2,6 +2,8 @@ vim.keymap.set("n", "<leader>gs", vim.cmd.Git);
 vim.keymap.set("n", "<leader>gb", function() vim.cmd('Git blame') end);
 
 -- Solve merge conflict with fugitive
+--  Git merge
+vim.keymap.set("n", "gm", "<cmd>Gvdiffsplit!<CR>");
 vim.keymap.set("n", "gf", "<cmd>diffget //2<CR>");
 vim.keymap.set("n", "gj", "<cmd>diffget //3<CR>");
 
